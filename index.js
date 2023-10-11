81d81b49ded85a657cd4080033678124eb8f2d08
AljawharahAlotaibi <aljwhraotb@gmail.com>
2023-10-11 16:42:40 +0300

    last commit

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
-//hey there
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = 3000;
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-const database = require("./database/database.js"); 
+1.       The property manager shall be able to view all available units.
+2.    The property manager shall be able to track all tenats.
+3.    The system shall help the property managers to know about facility management.//later on (facility management)
+4.    
+5.    The system shall help the property managers to know about maintenance request.// later on (PM maintenance requests)
+6.    The system shall collect the rents.
+7.    The tenants portal shall be able to process rental payments.//later on 
+8.    The tenants portal shall facilitate the maintenance request.//later on 
+9.    The tenants portal shall facilitate exploring the on-site facilities.  (ASK)
+       The tenants portal shall facilitate amenities reservation.//later on 
+10.    The tenants portal shall allow residents and staff to communicate via a channel. (ASK)
+11.    The manager shall be able to create a management account where They will be able to specify there are full name, phone number, and password.
+12.    The manager shall be able to manage their properties.
+13.    The manager shall be able to add a new property by identifying property name, age, number of units, number of floors, number of units on each floor, existence of the elevator, property address.
+14.    The manager shall be able to add units by specifying their information such as unit number, unit area, unit age, number of rooms, number of living rooms, number of bathrooms, and rent price.
+15.    The manager shall be able to Identify facilities of a property (facility name, facility floor number, reservation fee, facility rental method (daily, weekly, monthly))
 
 
-app.use('/', express.static(path.join(__dirname, 'public', 'login')));
-app.use('/signup', express.static(path.join(__dirname, 'public', 'signup')));
 
-app.get('/login', (req, res) => {
-  const { username, password } = req.query;
-  const user = {
-    username : username,
-    password : password
-  }
-  database.authenticate(user)
-  .then((result) => {
-    if(result.length > 0){
-      res.json(result);
-    }
-    else{
-      res.redirect('/?error=true');
-    }
-  }
-  )
-});
 
-app.get('/submitSignup', (req, res) => {
-  const { username, password } = req.query;
-  const user = {
-    username : username,
-    password : password
-  }
-  database.signup(user)
-  .then((result) => {
-    if(result){
-      res.json("user created! please login");
-    }
-    else{
-      res.redirect('/signup?error=true');
-    }
-  }
-  )
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+
+
+Non-functional req:
+
+
+ Design constraints:
+
+. Mobile application, the system shall operate on android and iOS platforms. 
+. Arabic language.
+
+
+
+
+
+1- 
